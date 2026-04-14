0e7bc2e161d9d0727b80086048a47f5c8867734e
Martin Angulo <chavezangulo9@gmail.com>
2026-04-14 14:16:29 -0600

    Ma

--- Inventario.BL/GestorInventario.cs
+++ Inventario.BL/GestorInventario.cs
@@ -9,7 +9,12 @@
             listaProductos = new List<MODEL.Producto>() { 
                 new MODEL.Producto { Id = 1, Nombre = "Laptop", Categoria = "Tecnología", Precio = 999.99, Stock = 10 },
                 new MODEL.Producto { Id = 2, Nombre = "Smartphone", Categoria = "Papelería", Precio = 499.99, Stock = 20 },
-
+                new MODEL.Producto { Id = 2, Nombre = "Smartphone", Categoria = "Papelería", Precio = 499.99, Stock = 20 },
+                new MODEL.Producto { Id = 1, Nombre = "Laptop", Categoria = "Tecnología", Precio = 999.99, Stock = 10 },
+                new MODEL.Producto { Id = 1, Nombre = "Laptop", Categoria = "Tecnología", Precio = 999.99, Stock = 10 },
+                new MODEL.Producto { Id = 2, Nombre = "Smartphone", Categoria = "Papelería", Precio = 499.99, Stock = 20 },
+                new MODEL.Producto { Id = 2, Nombre = "Smartphone", Categoria = "Papelería", Precio = 499.99, Stock = 20 },
+                new MODEL.Producto { Id = 1, Nombre = "Laptop", Categoria = "Tecnología", Precio = 999.99, Stock = 10 },
             };
         }
 
